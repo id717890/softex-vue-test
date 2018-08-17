3ebf270392472811b5d9e0df0a0607de1fd36539
YusupovZA <jusupovz@gmail.com>
2018-08-17 18:26:44 +0500

    remove temp code

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
 <template>
   <v-app dark>
-    <!-- <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer> -->
-    <v-toolbar
-      app
-    >
-      <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
-      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn> -->
-      <!-- <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn> -->
-      <!-- <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn> -->
-      <v-toolbar-title>
+    <v-toolbar app>
+      <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer;" v-text="title"></router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -58,29 +22,10 @@
           <v-icon left>{{item.icon}}</v-icon>
         </v-btn>
       </v-toolbar-items>
-      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn> -->
     </v-toolbar>
     <v-content style="min-height: 1500px">
       <router-view/>
     </v-content>
-    <!-- <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer> -->
     <v-footer :fixed="fixed" app>
       <span>&copy; 2018</span>
     </v-footer>
@@ -98,16 +43,7 @@ export default {
   },
   data () {
     return {
-      // clipped: false,
-      // drawer: true,
       fixed: false,
-      // items: [{
-      //   icon: 'bubble_chart',
-      //   title: 'Inspire'
-      // }],
-      // miniVariant: false,
-      // right: true,
-      // rightDrawer: false,
       title: 'SOFTEX-VUE'
     }
   },
