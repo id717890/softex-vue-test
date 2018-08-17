759f6fce54a7c7cc6532335753e22fae000976b8
YusupovZA <jusupovz@gmail.com>
2018-08-17 16:41:39 +0500

    clear template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,8 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
+        <blockquote class="display-1">
+          &#8220;Test job for interviewing&#8221;
         </blockquote>
       </v-layout>
     </v-slide-y-transition>
